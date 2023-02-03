e4329520c63cb079c3a2ca217c29bfaa2ba9a52d
Thiago <thiagolinog8@gmail.com>
2023-02-03 08:57:41 -0300

    Fix preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({ edge: true }),
-		inlineStyleThreshold: 5000,
-	},
-}
+		inlineStyleThreshold: 5000
+	}
+};
 
-export default config
+export default config;
